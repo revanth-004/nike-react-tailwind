fc877febc2817deeece3c99a1d9603f874917275
Revanth kumar S <revanthsureshlearn@gmail.com>
2024-11-05 16:16:48 +0530

    first-commit

---
+++ src/components/Nav.jsx
@@ -0,0 +1,44 @@
+import React from "react";
+import { headerLogo } from "../assets/images";
+import { hamburger } from "../assets/icons";
+import { navLinks } from "../constants";
+
+const Nav = () => {
+  return (
+    <header className="padding-x py-7 absolute z-10 w-full">
+      <nav className="flex justify-between items-center max-container bg-opacity-50 backdrop-blur-md">
+        <a href="/">
+          <img
+            src={headerLogo}
+            alt="Logo"
+            width={124}
+            height={30}
+            className="m-0 w-[124px] h-[30px]"
+          />
+        </a>
+        <ul className="flex-1 flex justify-center items-center gap-16 max-lg:hidden">
+          {navLinks.map((item) => (
+            <li key={item.label}>
+              <a
+                href={item.href}
+                className="font-montserrat  leading-normal text-lg text-slate-gray "
+              >
+                {item.label}
+              </a>
+            </li>
+          ))}
+        </ul>
+        <div className="flex gap-2 text-lg leading-normal font-medium font-montserrat max-lg:hidden wide:mr-24">
+          <a href="#contact-us">Sign in</a>
+          <span>/</span>
+          <a href="/">Explore now</a>
+        </div>
+        <div className="hidden max-lg:block">
+          <img src={hamburger} alt="hamburger" width={25} height={25} />
+        </div>
+      </nav>
+    </header>
+  );
+};
+
+export default Nav;
---
+++ src/sections/Services.jsx
@@ -0,0 +1,17 @@
+import { services } from "../constants";
+import { ServiceCard } from "../components";
+
+const Services = () => {
+  return (
+    <section
+      id="services"
+      className="max-container flex justify-center flex-wrap gap-9"
+    >
+      {services.map((service) => (
+        <ServiceCard key={service.label} {...service} />
+      ))}
+    </section>
+  );
+};
+
+export default Services;
